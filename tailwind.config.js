aaf3edcca43e2fd54764f792ffdd6c6ef33d61d5
sumon mia <sakimahmedsumon@gmail.com>
2025-04-18 11:14:58 +0600

    all file added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",   // adjust this path based on your project structure
+    "./index.html"     // add if you have an index.html in the public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
